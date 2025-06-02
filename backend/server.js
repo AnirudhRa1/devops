f3e83f1f777fe4e2bafc72dbee3dfa1b3d077015
Anirudh <anirudhanirudh982@gmail.com>
2025-06-02 10:20:29 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.get('/metrics', async (req, res) => {
 
 app.use(express.json());
 app.use(cors({
-  origin: ['https://devops-steel-psi.vercel.app','http://localhost:8080'],
+  origin: ['https://devops-steel-psi.vercel.app','http://localhost:8080','https://preview--simple-blog-world.lovable.app'],
   credentials: true
 }));
 
